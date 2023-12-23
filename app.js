59b77c400c74bd52410da35d3d5cbae6cad9363f
GustavoNery88 <gusta123sam@gmail.com>
2023-12-23 10:43:30 -0300

    save

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ mongoose.connect(process.env.MONGO_URL)
 });
 
 // Porta de acesso
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3333
+const PORT = process.env.PORT
 
 // Home page
 app.get('/', (req, res) => {
